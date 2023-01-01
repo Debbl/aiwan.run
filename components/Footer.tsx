53a71c08b5e59bf97be36ec42ec6828104d3fab7
Debbl <me@aiwan.run>
2023-01-01 21:47:31 +0800

    update: copt year

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,7 +2,7 @@ const Footer: React.FC = () => {
   return (
     <div className="flex mt-6 flex-col justify-center text-center text-gray-500">
       <div>
-        <a href="https://github.com/Debbl">&copy;2020 - 2022 By Debbl</a>
+        <a href="https://github.com/Debbl">&copy;2020 - 2023 By Debbl</a>
       </div>
       <div>
         <a href="">备案号：鄂ICP备19030152号-3</a>
