1ea50e6f33a9f478a50b47b40b3326c4436398db
Debbl <me@aiwan.run>
2023-11-06 21:21:24 +0800

    feat: sanpack

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 import createMDX from "@next/mdx";
 import remarkFrontmatter from "remark-frontmatter";
 import reamrkHeaderify from "remark-headerify";
+import rehypeMdxCodeProps from "rehype-mdx-code-props";
 import remarkMdxFrontmatter from "remark-mdx-frontmatter";
 
 /** @type {import('next').NextConfig} */
@@ -19,7 +20,7 @@ const withMDX = createMDX({
       reamrkHeaderify,
       [remarkMdxFrontmatter, { name: "metadata" }],
     ],
-    rehypePlugins: [],
+    rehypePlugins: [rehypeMdxCodeProps],
   },
 });
 
--- src/mdx-components.tsx
+++ src/mdx-components.tsx
@@ -1,6 +1,23 @@
 import type { MDXComponents } from "mdx/types";
-import Main from "./app/blog/(posts)/components/Main";
+import type { SandpackInternal } from "@codesandbox/sandpack-react/types";
+import Main from "~/app/blog/(posts)/components/Main";
+import Sandpack from "~/app/blog/(posts)/components/Sandpack";
 import Header from "~/app/blog/(posts)/components/Header";
+
+interface SandpackChildrenProps {
+  filename?: string;
+  children: { props: { children: string } };
+}
+
+type SandpackInternalParams = Parameters<SandpackInternal>[0];
+
+interface SandpackProps extends SandpackInternalParams {
+  children:
+    | Array<{
+        props: SandpackChildrenProps;
+      }>
+    | { props: SandpackChildrenProps };
+}
 
 export function useMDXComponents(components: MDXComponents): MDXComponents {
   return {
@@ -15,6 +32,26 @@ export function useMDXComponents(components: MDXComponents): MDXComponents {
 
     Main: ({ children }: { children: React.ReactNode }) => {
       return <Main>{children}</Main>;
+    },
+
+    Sandpack: (props: SandpackProps) => {
+      const { children, ..._props } = props;
+
+      const files: Record<string, string> = {};
+
+      if (Array.isArray(children)) {
+        children.forEach((child) => {
+          const filename = child.props.filename || "index.js";
+          const fileContent = child.props.children.props.children;
+          files[filename] = fileContent;
+        });
+      } else {
+        const filename = children.props.filename || "index.js";
+        const fileContent = children.props.children.props.children;
+        files[filename] = fileContent;
+      }
+
+      return <Sandpack files={files} {..._props} />;
     },
 
     ...components,
