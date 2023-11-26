49d26856a2b48ea03a80aa0869dc9d23d5b6e5d9
Brendan Dash <me@aiwan.run>
2023-11-26 17:36:00 +0800

    feat: add footer

--- src/app/blog/(posts)/layout.tsx
+++ src/app/blog/(posts)/layout.tsx
@@ -1,7 +1,18 @@
+import Link from "next/link";
+
 export default function MarkdownLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <>{children}</>;
+  return (
+    <>
+      {children}
+      <footer className="px-1 py-3 md:px-10 lg:px-32 xl:px-64">
+        <Link href="/blog" className="text-gray-600 underline">
+          cd ..
+        </Link>
+      </footer>
+    </>
+  );
 }
