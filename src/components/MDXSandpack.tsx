552cc053e85c956f49c6444e837b8e2a402de447
Brendan Dash <me@aiwan.run>
2023-12-06 15:11:01 +0800

    feat: sandpack dark mode

--- src/components/MDXSandpack.tsx
+++ src/components/MDXSandpack.tsx
@@ -1,11 +1,16 @@
 "use client";
 import type { SandpackInternal } from "@codesandbox/sandpack-react";
 import { Sandpack } from "@codesandbox/sandpack-react";
+import { useTheme } from "next-themes";
 
 const MDXSandpack: SandpackInternal = (props) => {
+  const { theme: _theme } = useTheme();
+  const theme = _theme === "dark" ? "dark" : "light";
+
   return (
     <div className="m-4">
       <Sandpack
+        theme={theme}
         options={{
           showConsoleButton: true,
         }}
