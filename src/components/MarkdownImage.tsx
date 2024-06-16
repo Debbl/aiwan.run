3dbb1ba53267b89b31d752bbe37087aa0d1359b7
Brendan Dash <me@aiwan.run>
2024-06-16 12:59:18 +0800

    fix: init MediumZoom

--- src/components/MarkdownImage.tsx
+++ src/components/MarkdownImage.tsx
@@ -1,14 +1,21 @@
+/* eslint-disable @next/next/no-img-element */
 "use client";
 import type { ImgHTMLAttributes } from "react";
-import { useRef } from "react";
+import { useEffect, useRef } from "react";
+import type { Zoom } from "medium-zoom";
 import MediumZoom from "medium-zoom";
 
 export function MarkdownImage(props: ImgHTMLAttributes<HTMLImageElement>) {
   const imgEl = useRef<HTMLImageElement>(null);
+  const zoom = useRef<Zoom>();
+
+  useEffect(() => {
+    zoom.current = MediumZoom(imgEl.current!);
+  }, []);
 
   const handleClick = () => {
-    MediumZoom(imgEl.current!).open();
+    zoom.current!.open();
   };
-  // eslint-disable-next-line @next/next/no-img-element
+
   return <img ref={imgEl} src={props.src} onClick={handleClick} />;
 }
