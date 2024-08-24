93d6c533d6f5e7f95b5881b5dc2fbacada005f43
Brendan Dash <me@aiwan.run>
2024-08-24 23:43:17 +0800

    feat: add post title

--- src/app/(MDX)/posts/[slug]/page.tsx
+++ src/app/(MDX)/posts/[slug]/page.tsx
@@ -1,8 +1,23 @@
 import { getMDXComponent } from "mdx-bundler/client";
+import type { Metadata } from "next";
 import { getMDXComponents } from "~/components/MDX";
 import { allPosts } from "~/data";
 
 const posts = allPosts.filter((p) => p.category === "posts");
+
+export function generateMetadata({
+  params,
+}: {
+  params: { slug: string };
+}): Metadata {
+  const post = posts.find((p) => p.slug === params.slug)!;
+
+  if (!post) return {};
+
+  return {
+    title: post.frontmatter.title,
+  };
+}
 
 export function generateStaticParams() {
   return posts.map((post) => ({
