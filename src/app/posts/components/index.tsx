d04aa957d72a3f0fd83bd124606f7fef151a2a9a
Brendan Dash <me@aiwan.run>
2024-08-29 20:28:52 +0800

    fix: some css

--- src/app/posts/components/index.tsx
+++ src/app/posts/components/index.tsx
@@ -15,21 +15,16 @@ export function getMDXComponents(): MDXComponents {
     h4: (props) => <HeadingLink tag="h4" {...props} />,
     h5: (props) => <HeadingLink tag="h5" {...props} />,
     h6: (props) => <HeadingLink tag="h6" {...props} />,
-    ul: (props) => (
-      <ul className="mt-6 list-disc first:mt-0 ltr:ml-6 rtl:mr-6" {...props} />
-    ),
+    ul: (props) => <ul className="ml-6 mt-6 list-disc first:mt-0" {...props} />,
     ol: (props) => (
-      <ol
-        className="mt-6 list-decimal first:mt-0 ltr:ml-6 rtl:mr-6"
-        {...props}
-      />
+      <ol className="ml-6 mt-6 list-decimal first:mt-0" {...props} />
     ),
     li: (props) => <li className="my-2" {...props} />,
     blockquote: (props) => (
       <blockquote
         className={cn(
           "mt-6 border-gray-300 mb-4 italic px-4 border-l-2 text-gray-700 dark:border-gray-700 dark:text-gray-400",
-          "first:mt-0 ltr:border-l-2 ltr:pl-6 rtl:border-r-2 rtl:pr-6",
+          "first:mt-0 border-l-2 pl-6",
         )}
         {...props}
       />
@@ -57,7 +52,7 @@ export function getMDXComponents(): MDXComponents {
       return <Sandpack files={files} {..._props} />;
     },
     Image,
-    pre: (props: any) => <Pre {...props} />,
+    pre: Pre,
     code: Code,
   };
 }
