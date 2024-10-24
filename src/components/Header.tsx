fa9aca2ae9fca90d211198795a5abfd409c59802
Brendan Dash <me@aiwan.run>
2024-10-24 13:24:18 +0800

    feat: add slides

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -18,6 +18,11 @@ const nav: {
     "data-umami-event": "click-blog-link",
   },
   {
+    "url": "/slides",
+    "name": "Slides",
+    "data-umami-event": "click-slides-link",
+  },
+  {
     "url": "/feed.xml",
     "name": "RSS",
     "data-umami-event": "click-rss-link",
