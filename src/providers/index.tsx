dece5851e16c6dc246028f39375a580fad63f20e
Brendan Dash <me@aiwan.run>
2025-01-01 15:34:04 +0800

    fix: motion provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,4 +1,4 @@
-import { domAnimation, LazyMotion } from "framer-motion";
+import { domAnimation, LazyMotion } from "motion/react";
 import { ThemeProvider } from "next-themes";
 
 export default function Providers({ children }: { children: React.ReactNode }) {
