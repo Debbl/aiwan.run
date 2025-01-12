e7a1abb5e4a3182cd48f91dc32bda3c3acd496de
Brendan Dash <me@aiwan.run>
2025-01-12 16:08:23 +0800

    chore: posts page align-center

--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -36,33 +36,35 @@ export default async function Page() {
       </div>
 
       <main className="relative flex-1 overflow-y-scroll">
-        <div className="mx-auto w-fit">
-          {postsByCategory.map((category) => (
-            <div className="mt-8" key={category.title} title={category.title}>
-              <h2 className="text-3xl font-bold">{category.title}</h2>
-              <ul className="mt-4 flex flex-col gap-y-2">
-                {category.posts.map((post) => (
-                  <a
-                    className="opacity-60 hover:opacity-100"
-                    href={post.url}
-                    key={post.slug}
-                  >
-                    <li
-                      className="text-gray-900 hover:text-primary dark:text-gray-50 dark:hover:text-primary"
-                      data-umami-event={`click-posts-${post.slug}`}
+        <div className="flex min-h-full w-full items-center justify-center">
+          <div className="flex w-fit flex-col items-start justify-center gap-y-8 py-8">
+            {postsByCategory.map((category) => (
+              <div key={category.title} title={category.title}>
+                <h2 className="text-3xl font-bold">{category.title}</h2>
+                <ul className="mt-4 flex flex-col gap-y-2">
+                  {category.posts.map((post) => (
+                    <a
+                      className="opacity-60 hover:opacity-100"
+                      href={post.url}
+                      key={post.slug}
                     >
-                      <span>{post.frontmatter.title}</span>
-                      <span className="ml-4 text-xs text-gray-500">
-                        {format(post.frontmatter.date, "MMM-dd")}
-                        {" · "}
-                        {post.frontmatter.duration}
-                      </span>
-                    </li>
-                  </a>
-                ))}
-              </ul>
-            </div>
-          ))}
+                      <li
+                        className="text-gray-900 hover:text-primary dark:text-gray-50 dark:hover:text-primary"
+                        data-umami-event={`click-posts-${post.slug}`}
+                      >
+                        <span>{post.frontmatter.title}</span>
+                        <span className="ml-4 text-xs text-gray-500">
+                          {format(post.frontmatter.date, "MMM-dd")}
+                          {" · "}
+                          {post.frontmatter.duration}
+                        </span>
+                      </li>
+                    </a>
+                  ))}
+                </ul>
+              </div>
+            ))}
+          </div>
         </div>
       </main>
     </>
