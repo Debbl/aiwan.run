b51213ddbc483ae6fef7eadcc0e0b1c8d933bc9c
Brendan Dash <me@aiwan.run>
2025-03-20 14:07:09 +0800

    perf: use webp replace png

--- src/logos/index.ts
+++ src/logos/index.ts
@@ -1,14 +1,14 @@
 import ai from "./ai.svg";
 import biliShortLink from "./bili-shortlink.svg";
-import bubbleWrap from "./bubble-wrap.png";
+import bubbleWrap from "./bubble-wrap.webp";
 import car from "./car.svg";
 import codeDiff from "./code-diff.svg";
 import emoji from "./emoji.svg";
-import gameOfLife from "./game-of-life.png";
+import gameOfLife from "./game-of-life.webp";
 import minesweeper from "./minesweeper.svg";
 import peppa from "./peppa.svg";
 import rm from "./rm.svg";
-import tictactoe from "./tictactoe.png";
+import tictactoe from "./tictactoe.webp";
 import tools from "./tools.svg";
 import v from "./v.svg";
 
