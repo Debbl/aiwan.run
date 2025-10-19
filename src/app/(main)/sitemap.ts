445f86e00cecbf92691d562c130542597641befd
Brendan Dash <me@aiwan.run>
2025-10-19 13:45:04 +0800

    fix: sitemap

--- src/app/(main)/sitemap.ts
+++ src/app/(main)/sitemap.ts
@@ -3,5 +3,5 @@ import { generateStaticSitemap } from '../[lang]/sitemap'
 export const dynamic = 'force-static'
 
 export default function sitemap() {
-  return generateStaticSitemap('zh')
+  return generateStaticSitemap('en')
 }
--- src/app/[lang]/sitemap.ts
+++ src/app/[lang]/sitemap.ts
@@ -8,25 +8,92 @@ export function generateStaticParams() {
   return [{ lang: 'zh' }]
 }
 
-export function generateStaticSitemap(
-  lang: 'en' | 'zh' = 'en',
-): MetadataRoute.Sitemap {
-  const lastModified = posts.at(-1)?.data.date
-
-  const postsSitemap = posts
+function getPostSitemap(lang: 'en' | 'zh' = 'en'): MetadataRoute.Sitemap {
+  return posts
     .map((rawPost) => {
       const post = source.getPage(rawPost.slugs, lang)
 
       if (!post) return null
 
+      const url = `${WEBSITE.domain}${lang === 'zh' ? post.url : post.url.slice(3)}`
       return {
-        url: `${WEBSITE.domain}${post.url}`,
+        url,
         lastModified: post.data.date,
         changeFrequency: 'weekly',
         priority: 0.8,
       } as const satisfies MetadataRoute.Sitemap[number]
     })
     .filter(Boolean)
+}
+
+export function generateStaticSitemap(
+  lang: 'en' | 'zh' = 'en',
+): MetadataRoute.Sitemap {
+  const lastModified = posts.at(-1)?.data.date
+
+  const postsSitemap = getPostSitemap(lang)
+
+  if (lang === 'zh') {
+    return [
+      {
+        url: `${WEBSITE.domain}/zh`,
+        lastModified,
+        changeFrequency: 'weekly',
+        priority: 1,
+      },
+      {
+        url: `${WEBSITE.domain}/zh/posts`,
+        lastModified,
+        changeFrequency: 'weekly',
+        priority: 0.95,
+      },
+      {
+        url: `${WEBSITE.domain}/zh/feed.xml`,
+        lastModified,
+        changeFrequency: 'weekly',
+        priority: 0.9,
+      },
+      {
+        url: `https://tools.aiwan.run`,
+        lastModified,
+        changeFrequency: 'weekly',
+        priority: 0.9,
+      },
+      {
+        url: `https://slides.aiwan.run`,
+        lastModified,
+        changeFrequency: 'weekly',
+        priority: 0.9,
+      },
+      {
+        url: `https://game.aiwan.run`,
+        lastModified,
+        changeFrequency: 'weekly',
+        priority: 0.9,
+      },
+      {
+        url: `https://emoji.aiwan.run`,
+        lastModified,
+        changeFrequency: 'weekly',
+        priority: 0.9,
+      },
+      ...postsSitemap,
+      {
+        url: `${WEBSITE.domain}/zh/posts`,
+        lastModified: posts.at(-1)?.data.date,
+        changeFrequency: 'weekly',
+        priority: 0.75,
+        images: posts.map((post) => {
+          const slugs = post.slugs
+          const ogImgPath = [...slugs.slice(0, -1), `${slugs.at(-1)}.png`].join(
+            '/',
+          )
+
+          return `${WEBSITE.domain}/posts/og/${ogImgPath}`
+        }),
+      },
+    ]
+  }
 
   return [
     {
